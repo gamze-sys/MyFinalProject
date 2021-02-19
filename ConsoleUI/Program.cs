28871b411a8dc39bbad095de77f13bfb5003d4f0
unsalgamze1377@gmail.com <unsalgamze1377@gmail.com>
2021-02-19 23:37:50 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+using Business.Concrete;
+
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+
+namespace ConsoleUI
+{
+    //SOLID-Yaptigimiz hareket O harfi=Open Closed Principle
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal());
+
+            foreach (var product in productManager.GetByUnitPrice(40,100))
+            {
+                Console.WriteLine(product.ProductName);
+            }
+        }
+    }
+}
---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+using Entities.Abstract;
+using Entities.Concrete;
+
+namespace DataAccess.Abstract
+{
+    //generic constraint
+    //class:referans tip olabilir demektir
+    //IEntity:IEntity olabilir veya IEntity implemente eden bir nesne olabilir
+    //new():new'lenebilir olmali
+
+   public interface IEntityRepository<T>where T:class ,IEntity,new()
+    {
+        List<T> GetAll(Expression<Func<T,bool>>filter=null);
+        T Get(Expression<Func<T,bool>>filter);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        
+
+    }
+}
